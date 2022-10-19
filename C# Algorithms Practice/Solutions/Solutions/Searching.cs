126e65b7dd335de491273517d9ef6c5a364f9d43
WahajUlHaq <ulhaqwahaj@gmail.com>
2022-10-19 10:07:34 +0500

    Searching/Linear-Search

---
+++ C# Algorithms Practice/Solutions/Solutions/Searching.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Searching
+{
+  public class LinearSearch
+  {
+    public int search(int[] arrayOfNumber, int numberToFind)
+    {
+      int lengthOfArray = arrayOfNumber.Length;
+
+      for (int i = 0; i < lengthOfArray; i++)
+      {
+        if (arrayOfNumber[i] == numberToFind)
+        {
+          return i;
+        }
+      }
+      
+      return -1;
+    }
+  }
+  
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      int[] arrayOfNumber = { 22, 33, 44, 10, 50 };
+      int numberToFind = 10;
+
+      // Linear Search Algorithm
+      LinearSearch linearSearch = new LinearSearch();
+      int linearSearchResult = linearSearch.search(arrayOfNumber, numberToFind);
+      
+      Console.WriteLine(linearSearchResult);
+    }
+  }
+}
